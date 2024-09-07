010c854f2adffba25016b4e328a3346fe4ce7e21
Fahim <69208613+fhsourav@users.noreply.github.com>
2024-09-08 00:20:05 +0600

    used javascript and css to design the calculator for efficient code

--- script.js
+++ script.js
@@ -26,5 +26,77 @@ function operate(operator, num1, num2) {
 	}
 }
 
-const digit1 = document.querySelector("#1");
-digit1.addEventListener("")
+const calculator = document.querySelector("#calculator");
+
+const display = document.createElement("div");
+display.id = "display";
+
+const displayNumber = document.createElement("p");
+displayNumber.id = "displayNumber";
+display.appendChild(displayNumber);
+
+const displayOperator = document.createElement("p");
+displayOperator.id = "displayOperator";
+display.appendChild(displayOperator);
+
+calculator.appendChild(display);
+
+const buttons = document.createElement("div");
+buttons.id = "buttons";
+calculator.appendChild(buttons);
+
+const digits = document.createElement("div");
+digits.id = "digits";
+buttons.appendChild(digits);
+
+const operators = document.createElement("div");
+operators.id = "operators";
+buttons.appendChild(operators);
+
+for (let i = 9; i >= 0; i--) {
+	const digit = document.createElement("button");
+	digit.textContent = i;
+	digit.classList.add("digit");
+	digit.addEventListener("click", () =>{
+		displayNumber.textContent += digit.textContent;
+	});
+	digits.appendChild(digit);
+}
+
+const operatorList = [
+	{
+		sign: '/',
+		id: 'divide'
+	},
+	{
+		sign: '*',
+		id: 'multiply'
+	},
+	{
+		sign: '-',
+		id: 'subtract'
+	},
+	{
+		sign: '+',
+		id: 'add'
+	},
+	{
+		sign: '=',
+		id: 'equals'
+	},
+	{
+		sign: 'C',
+		id: 'clear'
+	}
+]
+
+operatorList.forEach(element => {
+	const operator = document.createElement("button");
+	operator.textContent = element.sign;
+	operator.id = element.id;
+	operator.classList.add("operator");
+	operator.addEventListener("click", () =>{
+		displayOperator.textContent = operator.textContent;
+	});
+	operators.appendChild(operator);
+});
